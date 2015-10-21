2ce55e1a2d5732a50e24f9c889a7833ecfc38648
Ruslan Y <maestroprog@gmail.com>
2015-10-22 01:49:22 +0500

    программирую разделенную отправку/получение данных

--- src/common/Net.php
+++ src/common/Net.php
@@ -15,8 +15,13 @@ abstract class Net
 {
     const DATA_RAW = 0;
     const DATA_JSON = 1;
-    const DATA_CHUNK = 2;
-    const DATA_CHUNK_CONTROL = 4;
+    const DATA_INT = 2;
+    const DATA_FLOAT = 4;
+    const DATA_STRING = 8;
+    const DATA_ARRAY = 16;
+    const DATA_EXTENDED = 32;
+    const DATA_CHUNK = 64;
+    const DATA_CHUNK_CONTROL = 128;
     /**
      * @var int type of socket
      */
@@ -50,6 +55,22 @@ abstract class Net
      */
     private $event_receive;
 
+    /* private variables */
+
+    /**
+     * @var int
+     * auto increment message id
+     * #for send
+     */
+    private $mid = 0;
+
+    /**
+     * @var array
+     * chunk data buffer
+     * #for read
+     */
+    private $buffer_chunk = [];
+
     public function __construct($config)
     {
         foreach ($config as $key => $val)
@@ -59,18 +80,23 @@ abstract class Net
     public function doReceive()
     {
         // read message meta
-        if (($data = $this->_read(4)) !== false) {
-            list($length, $flag) = unpack('nn', $data);
+        if (($data = $this->_read(8)) !== false) {
+            list($length, $mid, $flag) = unpack('nNn', $data);
             out('read length ' . $length);
-            out('flag ' . $flag);
-            if ($flag & 0x00ff & self::DATA_CHUNK) {
+            out('flag ' . decbin($flag));
+            if (($data = $this->_read($length, true)) !== false) {
+                out('data retrieved');
+            } else {
+                out('cannot retrieve data');
+            }
+            if ($flag & self::DATA_CHUNK) {
                 $chunks = [];
                 $control = false;
                 $size = 0;
                 $try = 0;
                 do {
                     if (!($flag & self::DATA_CHUNK_CONTROL)) {
-                        $chunks[$flag & 0xff00 >> 8] = $data;
+                        $this->buffer_chunk[$mid][($flag & 0xff00) >> 8] = $data;
                     } else {
                         $size = (int)$data;
                     }
@@ -79,10 +105,6 @@ abstract class Net
                     }
                 } while (!$control || count($chunks) < $size);
             } else {
-                if (($data = $this->_read($length, true)) !== false) {
-                } else {
-                    out('cannot retrieve data');
-                }
             }
         }
         return false;
@@ -95,12 +117,42 @@ abstract class Net
 
     public function send($data)
     {
-        if (is_string($data)) {
-            $type = 0;
-        } else {
-            $data = json_encode($data);
-            $type = 1;
+        $this->mid++; // non-stable integer
+        $flag = 0;
+        switch (gettype($data)) {
+            case 'boolean':
+                trigger_error('Boolean data type cannot be transmitted', E_USER_WARNING);
+                return false;
+                break;
+            case 'integer':
+                $flag |= self::DATA_INT;
+                break;
+            case 'double':
+                $flag |= self::DATA_FLOAT;
+                break;
+            case 'array':
+                $flag |= self::DATA_ARRAY | self::DATA_JSON;
+                break;
+            case 'object':
+                $flag |= self::DATA_EXTENDED | self::DATA_JSON;
+                trigger_error('Values of type Object cannot be transmitted on current Net version', E_USER_WARNING);
+                return false;
+                break;
+            case 'resource':
+                trigger_error('Values of type Resource cannot be transmitted on current Net version', E_USER_WARNING);
+                return false;
+                break;
+            case 'NULL':
+                trigger_error('Null data type cannot be transmitted', E_USER_WARNING);
+                return false;
+                break;
+            case 'unknown type':
+                trigger_error('Values of Unknown type cannot be transmitted on current Net version', E_USER_WARNING);
+                return false;
+                break;
         }
+        if ($flag & self::DATA_JSON)
+            $data = json_encode($data);
         $length = strlen($data);
         if ($length >= 0xffff) {
             trigger_error('Big data size to send! I can split it\'s', E_USER_WARNING);
@@ -109,15 +161,15 @@ abstract class Net
             do {
                 $chunk = substr($data, $offset += 0xffff, 0xffff);
                 $length -= $len = strlen($chunk);
-                $this->_send($chunk, $len, $num++ << 8 | $type | self::DATA_CHUNK);
+                $this->_send($this->mid, $chunk, ($num++ << 8) | $flag | self::DATA_CHUNK);
                 if ($num > 255) {
-                    // 255 фреймов должно хватит на 16M
+                    // 255 фреймов должно хватить на 16M
                     trigger_error('Big chunk number!', E_USER_ERROR);
                 }
             } while ($length > 0);
-            $this->_send($num, strlen($num), self::DATA_CHUNK | self::DATA_CHUNK_CONTROL);
+            return $this->_send($this->mid, $num, $flag | self::DATA_CHUNK | self::DATA_CHUNK_CONTROL);
         } else {
-            $this->_send($data, $type);
+            return $this->_send($this->mid, $data, $flag);
         }
     }
 
@@ -177,7 +229,10 @@ abstract class Net
                 throw new \Exception('Socket read error: ' . socket_strerror(socket_last_error(socket_last_error($this->connection))), socket_last_error($this->connection));
             } elseif ($data === '') {
                 trigger_error('Socket read 0 bytes', E_USER_WARNING);
-                if (!$required || $try++ > 100) return false;
+                if (!$required || $try++ > 100) {
+                    trigger_error('Fail require read data', E_USER_ERROR);
+                    return false;
+                }
                 usleep(1000);
             } else {
                 $buffer .= $data;
@@ -188,10 +243,11 @@ abstract class Net
         return $buffer;
     }
 
-    private function _send($data, $length, $flag = 0)
+    private function _send($mid, $data, $flag = 0)
     {
-        $data = pack('nna*', $length, $flag, $data);
-        $length += 4;
+        $length = strlen($data);
+        $data = pack('nNna*', $length, $mid, $flag, $data);
+        $length += 8;
         $written = 0;
         do {
             $wrote = socket_write($this->connection, $data);
